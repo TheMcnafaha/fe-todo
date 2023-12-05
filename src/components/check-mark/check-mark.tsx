26109a99435b729cfe5c9a62566af9e7095e7e79
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-12-04 23:02:15 -0500

    added state checm simbol

--- src/components/check-mark/check-mark.tsx
+++ src/components/check-mark/check-mark.tsx
@@ -1,9 +1,27 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
 export interface CheckMarkProps {}
 
 export const CheckMark = component$<CheckMarkProps>((props) => {
+  const clicked = useSignal(false);
+  let className =
+    "flex h-[1.15rem] w-[1.15rem] items-center justify-center  rounded-full border-[1px]  border-gray-hover-blue  ";
+  if (clicked.value) {
+    className =
+      "flex h-[1.15rem] w-[1.15rem] items-center justify-center  rounded-full border-0  border-[1px] border-gray-hover-blue bg-gradient-to-br from-lg-cyan to-lg-magnenta ";
+  }
   return (
-    <div class="h-[1.15rem] w-[1.15rem]  rounded-full border-[1px]  border-gray-hover-blue  "></div>
+    <div
+      class={className}
+      onClick$={(e) => {
+        clicked.value = !clicked.value;
+      }}
+    >
+      {clicked.value && <CheckSymbol />}
+    </div>
   );
+});
+
+export const CheckSymbol = component$<CheckMarkProps>((props) => {
+  return <img src="/icon-check.svg" alt="checked" />;
 });
