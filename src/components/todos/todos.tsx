5386615ebbf369b6efb2c5597566621fa73cc2a1
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2023-12-22 00:15:25 -0500

    manual vanilla js solution hell

--- src/components/todos/todos.tsx
+++ src/components/todos/todos.tsx
@@ -1,4 +1,4 @@
-import { Signal, component$, useComputed$, useSignal, useTask$ } from "@builder.io/qwik";
+import { Signal, component$,$, useComputed$, useOnDocument, useSignal, useTask$ } from "@builder.io/qwik";
 import { Todo } from "../todo/todo";
 import { TodoObj } from "~/routes";
 import { StatusesBar } from "../statuses-bar/statuses-bar";
@@ -9,6 +9,7 @@ export interface TodosProps {
 
 export type TodoStatus="completed"|"all"|"active"
 export const Todos = component$<TodosProps>(({ todos }) => {
+  const dragging=useSignal(false)
   const status=useSignal<TodoStatus>("all")
   let filteredTodos=useComputed$( ( ) => { 
     if (status.value==="all") {
@@ -20,18 +21,38 @@ export const Todos = component$<TodosProps>(({ todos }) => {
     return todos.value.filter(todo=>todo.completed)
   })
   const itemsLeft=filteredTodos.value.length 
+  useOnDocument("load",$( ( ) => { 
+document.addEventListener("mouseup", (  ) => { 
+console.log("lol lil bro thinks he can program");
+dragging.value=false
+    })
+    const ul=document.getElementById("fav-ul")
+    console.log(ul);
+    ul!.addEventListener("mousedown", (e) => { 
+      const favStrg=e.target.innerText
+      const favId=todos.value.findIndex(todo=>todo.text===favStrg)
+console.log("her eig ",favId );
+      console.log(favStrg);
+      
+})
+    
+  }))
   return (
     <>
       <div class="mb-8">
+        <div id="lol" class=" h-[24px] absolute w-full z-40 bg-red-500 rounded-full"></div>
         <div class="rounded-t-lg rounded-b-lg">
-<ul class=" rounded-md border-dark-gray-blue drop-shadow-sm">
+<ul id="fav-ul" class=" rounded-md border-dark-gray-blue drop-shadow-sm"
+          >
           {filteredTodos.value.map((todo, i, e) => {
             const className = getTodoClass(i);
             return (
-              <li key={todo.text} class={className} onClick$={(e ) => { console.log("wizrad ", e.x, e.y); }} onMouseDown$={ (e) => { console.log("magic ",e);
-               }}  onMouseOut$={(e ) => { 
-                    console.log("goodbye ",e.target.innerText);
-                }}>
+              <li key={todo.text} class={className}  id={i.toString()} onClick$={(e ) => { console.log("wizrad ", e.x, e.y); }} 
+                  onMouseUp$={ ( e) => { 
+                    dragging.value=false
+                    console.log("letting gooooo");
+                    
+                  }}>
                 <Todo todo={todo} text={todo.text} todos={todos} />
               </li>
             );
@@ -59,3 +80,28 @@ function getTodoClass(index: number, ): string {
   const className= "bg-[white] dark:bg-dark-saturated-blue py-3 w-full border-t-[1px] border-t-light-gray-blue dark:border-t-darker-gray-blue"
   return className;
 }
+               //    onMouseDown$={ (e) => { 
+               //      if (dragging.value) {
+               //       return 
+               //      }
+               //    onsole.log("magic ",e);
+               //    dragging.value=true
+               //    const elem=document.getElementById("lol")
+               //    const rect=elem.getBoundingClientRect()
+               //    console.log("magic stuff ", rect);
+               //    console.log(elem);
+               //    // elem!.style.position="absolute"
+               //    elem!.style.top=e.y.toString()+"px"
+               //    
+               // }}
+               //    onMouseMove$={(e ) => { 
+               //      console.log("goodbye ",e.target.innerText);
+               //      if (dragging.value) {
+               //        const elem=document.getElementById("lol")
+               //    const rect=elem.getBoundingClientRect()
+               //    console.log("magic stuff ", rect);
+               //    console.log(elem);
+               //    // elem!.style.position="absolute"
+               //    elem!.style.top=e.y.toString()+"px"
+               //      }
+               //  }}>
